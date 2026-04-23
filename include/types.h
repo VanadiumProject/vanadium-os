6d589697d5a1a4fc57943f8e04fd5ebf710eba02
Julio Cesar Carvalho de Paula Souza <julio191096@gmail.com>
2026-04-22 21:07:42 -0300

    feat: bootloader: improved trap_handler
    - Now the trap handler uses 32 general purpose registers(x0-x31)
    Note: x0 is always 0
    - created trap_frame_t to be a able to save the previous state of the
      machine, it's good for debugging and makes possible for saving states

--- include/types.h
+++ include/types.h
@@ -1,6 +1,7 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+	
 typedef unsigned char		ui8_t;
 typedef unsigned short		ui16_t;
 typedef unsigned int		ui32_t;
@@ -14,5 +15,14 @@ typedef signed long long	i64_t;
 typedef unsigned long long	size_t;
 typedef unsigned long long	uintptr_t;
 typedef signed long long	intptr_t;
+
+typedef struct {
+	uintptr_t xregs[32];
+	uintptr_t mepc;
+	uintptr_t mstatus;
+	uintptr_t mcase;
+	uintptr_t mtval;
+	uintptr_t kernel_sp;
+} trap_frame_t;
 
 #endif
